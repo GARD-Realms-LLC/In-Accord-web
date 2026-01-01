65f2cc284da1990e508538e98b83ebf99b8fff0c
Doc Cowles <docrst@gmail.com>
2025-12-31 18:49:02 -0700

    Remove unused components and pages; refactor Sidebar and DashboardWrapper for improved state management and layout

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,11 +1,26 @@
 "use client";
 
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { setIsSidebarCollapsed } from '@/state';
 import { Menu } from 'lucide-react';
 import React from 'react';
 
 const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  }; 
+
+  const sidebarClasseNames = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 : md:w-16" : "w-72 md:w-64"
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+
   return (
-    <div>
+    <div className={sidebarClasseNames}>
 
     {/* TOP LOGO */}
 
@@ -13,10 +28,13 @@ const Sidebar = () => {
       <div>logo</div>
       <h3 className="font-extrabold text-2xl">IA</h3>
 
-    <button className="md-hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={() =>{}}>
-       <Menu className="w-4 h-4" />
-      </button>
-     </div>
+    <button className="md-hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" 
+      onClick={toggleSidebar} 
+      >
+      <Menu className="w-4 h-4" />
+    </button>
+    </div>
+
 
      {/* LINKS */}
 
