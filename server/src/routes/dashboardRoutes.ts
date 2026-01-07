c0952b590195bb74fbefba82d675ad7a464a6ce0
Doc Cowles <docrst@gmail.com>
2026-01-07 11:45:44 -0700

    feat: update dashboard route to use '/metrics' endpoint for metrics retrieval

--- server/src/routes/dashboardRoutes.ts
+++ server/src/routes/dashboardRoutes.ts
@@ -3,6 +3,6 @@ import { getDashboardMetrics } from "../controllers/dashboardController";
 
 const router = Router();
 
-router.get("/", getDashboardMetrics);
+router.get("/metrics", getDashboardMetrics); // http://localhost:8000/dashboard/metrics
 
 export default router;
