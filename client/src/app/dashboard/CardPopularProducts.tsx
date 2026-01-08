00c332a664f1e89077d7688b9a4b2f5724e210fc
Doc Cowles <docrst@gmail.com>
2026-01-07 19:23:03 -0700

    fix: enhance layout and structure in CardPopularProducts component

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -21,7 +21,7 @@ const CardPopularProducts  = () => {
                key={product.productId}
                className="flex items-center justify-between gap-3 px-5 py-7 border-b"
              >
-              <div>
+              <div className="flex items-center gap-3">
               <div> img </div>
               <div className="flex flex-col justify-between gap-1">
                 <div className="font-bold text-gray-700">{product.name}</div>
@@ -30,10 +30,17 @@ const CardPopularProducts  = () => {
                   ${product.price}
                 </span>
                 <span className="mx-2">|</span>
-                <div>rating</div>
+                <Rating />
               </div>
-            </div> 
+             </div> 
             </div>
+             
+             <div className="text-xs flex items-center gap-3">
+                 <button className="p-2 rounded-full bg-blue-100 text-blue-600 mr-2">
+                   <ShoppingBag className="w-4 h-4" />  
+                 </button>
+                  {Math.round(product.stockQuantity / 1000)}k Sold
+             </div>
           </div>
          ))}
         </div>
