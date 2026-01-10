cbc6393a44b0c88960dc933045a7db8cbb8cd4ce
Doc Cowles <docrst@gmail.com>
2026-01-10 00:28:59 -0700

    refactor(Home): update layout and add additional project information; clean up unused image element

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,20 +1,19 @@
 const Home = (props: Props) => {
     return (
-      <div className="flex flex-col items-center min-h-full gap-6 pb-6">
+      <div className="flex flex-col items-center h-full pb-6">
         <div className="flex flex-col justify-center items-center gap-6 flex-1">
           <h1 className="text-6xl font-bold">In-Accord</h1>
           <h2 className="text-2xl font-bold">The Premier Discord Customization an Management App.</h2>
           <h3 className="text-1xl font-bold">A standalone program which automates the installation, removal and miantenance of InAccord 
             a Discord Client Customisation App</h3>
           <h3 className="text-1xl font-bold">This project is cerated and Matained by all Adults! - In-Accord is based off the last avalable branch of GooseMod.</h3>
+          
+          <img 
+            src="https://pub-7d4119dd86a04c7bbdbcc230a9d161e7.r2.dev/Images/In-Accord%20working.png" 
+            alt="In-Accord" 
+            className="w-200 h-200 object-contain"
+          />
         </div>
-
-
-        <img 
-          src="https://pub-7d4119dd86a04c7bbdbcc230a9d161e7.r2.dev/Images/In-Accord%20working.png" 
-          alt="In-Accord" 
-          className="w-200 h-200 object-contain"
-        />
 
         {/* Sidebar Footer */}
         <div className="w-full bg-white dark:bg-gray-800 p-4 rounded-lg border border-gray-200 dark:border-gray-700">
