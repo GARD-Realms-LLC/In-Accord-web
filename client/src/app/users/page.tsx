fb76248a3ed1cd8f0569b0e4e46830eae9c635ad
Doc Cowles <docrst@gmail.com>
2026-01-17 20:13:03 -0700

    feat: remove header from Users page for layout simplification

--- client/src/app/users/page.tsx
+++ client/src/app/users/page.tsx
@@ -6,9 +6,7 @@ const Users = (props: Props) => {
     <HomePageWrapper>
       <div className="min-h-screen flex flex-col">
         <div className="flex-grow">
-          <h1 className="rounded-lg bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white p-4 text-2xl font-semibold text-center">
-            Users and Developers
-          </h1>
+          
           <div>
           </div>
         </div>
@@ -18,4 +16,6 @@ const Users = (props: Props) => {
 }
 
 export default Users
+
+
 
